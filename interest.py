7e3c8ca47982139d567a301132533a601c759dde
kunli1994 <43356064+kunli1994@users.noreply.github.com>
2018-12-06 23:23:07 -0500

    Update interest.py

--- interest.py
+++ interest.py
@@ -15,19 +15,29 @@ def create_labeled_data():
     # collect all data from the corpus
     interest = senseval.instances('interest.pos')
     # create labeled data
-    labeled_data = ...
+    labeled_data = [(wsd_features(instance),instance.senses[0]) for instance in interest]
+    print(len(labeled_data))
     return labeled_data
 
 
 def create_feature_sets(labeled_data):
     # create feature sets
-    ...
+
+
+
+    test_set, train_set = labeled_data[round(len(labeled_data) * 0.9):], labeled_data[:round(len(labeled_data) * 0.9)]
     return train_set, test_set
 
 
 def wsd_features(instance):
-    ...
-    return { ... }
+    # pre = {'pre':instance.context[instance.position - 1][0]}
+    # pos = {'pos':instance.context[instance.position + 1][0]}
+    # pre_tag = {'pre_tag':instance.context[instance.position - 1][1]}
+    # pos_tag = {'pos_tag':instance.context[instance.position + 1][1]}
+
+
+
+    return {'pre':instance.context[instance.position - 1][0],'pos':instance.context[instance.position + 1][0],'pre_tag':instance.context[instance.position - 1][1],'pos_tag':instance.context[instance.position + 1][1]}
 
 
 def make_instance(tagged_sentence):
@@ -38,16 +48,30 @@ def make_instance(tagged_sentence):
 
 def train_classifier(training_set):
     # create the classifier
-    ...
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+    return classifier
+
 
 
 def evaluate_classifier(classifier, test_set):
     # get the accuracy and print it
-    ...
+    print("Accuracy: ",nltk.classify.accuracy(classifier,test_set))
+
 
 
 def run_classifier(classifier):
-    ...
+    emma = nltk.corpus.gutenberg.sents('austen-emma.txt')
+    sents = [s for s in emma if 'interest' in s]
+    tag_sents = [nltk.pos_tag(sent) for sent in sents]
+    instances = [make_instance(tag_sent) for tag_sent in tag_sents]
+    for ins in instances:
+        ins.senses = classifier.classify(wsd_features(ins))
+        lst = [word for (word,tag) in ins.context]
+        lst[ins.position] = ins.senses
+        print(' '.join(lst))
+
+
+
 
 
 if __name__ == '__main__':
